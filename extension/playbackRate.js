e1703975b9c4eaf99a73025740d54da672097ade
Ryan Eppers <gitnix1337@gmail.com>
2017-11-17 11:41:25 -0500

    Refactored code to work exclusively with mutation observers (removed click event listeners). As a result, speed/menu for preview videos now get set correctly. Bumped minor version.

--- extension/playbackRate.js
+++ extension/playbackRate.js
@@ -2,82 +2,14 @@ const MAX_RATE = 4
 const MIN_RATE = 0.5
 const INCREMENT = 0.25
 
-const LECTURE_ITEM_LINK_SELECTION_COLOR = 'rgb(0, 94, 114)'
-const DETAIL_BUTTON = 'detail__continue-button'
 const MENU_ITEM_HIGHLIGHT = 'rgb(236, 82, 82)'
-
-// if the event.target.classList contains one of these classes
-// start mutation observer
-const CLASS_LIST = [
-	'fx',
-	'lecture__item__link__name',
-	'lecture__item__link__time',
-	'mr5',
-	'lecture__item__link',
-	'udi-play',
-	'udi-next',
-	'continue-button--btn__label--3H0BR',
-	'continue-button--responsive--3c3TI',
-	'play-icon',
-	'detail__continue-button',
-]
-
-const observeClick = event => {
-	const willObserve = () => {
-		// used to catch a pesky span on "continue to lecture" button
-		if (event.target.tagName === 'SPAN') {
-			if (ccClass(event.target)(DETAIL_BUTTON)) return true
-		}
-		// ensures mutation observer not run (and thus elements not removed) if click
-		// on current video list item on the video player page
-		// current is determined from background color of the list item (the blue background)
-		if (ccBackgroundColor(event.target)(LECTURE_ITEM_LINK_SELECTION_COLOR)) return false
-
-		return CLASS_LIST.some(string => {
-			if (isInClassList(string)(event.target)) return true
-		})
-	}
-	if (willObserve()) {
-		// if new video selected or some continue button clicked, run setup for next video
-		observeDocument()
-	}
-}
-// Udemy uses AJAX for everything which rebuilds HTML necessitating the rerunning of
-// mutation observers. As document is not rebuilt use event delegation to caputure clicks
-document.addEventListener('click', observeClick)
-
-// check if some computed value from node or its ancestors equals some expected value
-const nodeInChainMeetsCondition = node => computeFn => expected => nullNodeReturnVal => {
-	if (node == null || node.parentNode == null) return nullNodeReturnVal
-	if (computeFn(node) === expected) return true
-	if (node.parentNode.isEqualNode(document)) return false
-	return nodeInChainMeetsCondition(node.parentNode)(computeFn)(expected)(nullNodeReturnVal)
-}
-
-// let's get functional
-const applyFn = val => fn => fn(val)
-const getStyle = node => applyFn(node)(getComputedStyle)
-const getProp = obj => prop => obj[prop]
-const getPropOfStyle = node => prop => getProp(getStyle(node))(prop)
-
-const getBackgroundColor = node => getPropOfStyle(node)('backgroundColor')
-const isInClassList = className => node => getProp(node)('classList').contains(className)
-
-// cc means check chain (check recursively up the node chain)
-const ccBackgroundColor = node => color => nodeInChainMeetsCondition(node)(getBackgroundColor)(color)(false)
-const ccClass = node => className => nodeInChainMeetsCondition(node)(isInClassList(className))(className)(true)
 
 function initialize(video, menu, rateBtn) {
 	function getPlaybackRate(event) {
-		// if video made it to end, run setup for next video
-		if (event.type === 'ended') observeDocument()
-
 		chrome.storage.sync.get({ udemy_playback_rate: 1 }, obj => {
-			if (event.type === 'loadeddata') {
-				// gotta love algebra
-				let itemIndexPosition = (obj.udemy_playback_rate - MAX_RATE) / INCREMENT / -1
-				setMenuItemsHTML(menu.children[itemIndexPosition])
-			}
+			// gotta love algebra
+			let itemIndexPosition = (obj.udemy_playback_rate - MAX_RATE) / INCREMENT / -1
+			setMenuItemsHTML(menu.children[itemIndexPosition], menu)
 			video.playbackRate = obj.udemy_playback_rate
 		})
 	}
@@ -89,7 +21,7 @@ function initialize(video, menu, rateBtn) {
 				let newRate = obj.udemy_playback_rate + INCREMENT
 				if (newRate > MAX_RATE) newRate = MIN_RATE
 				let itemIndexPosition = (newRate - MAX_RATE) / INCREMENT / -1
-				setMenuItemsHTML(menu.children[itemIndexPosition])
+				setMenuItemsHTML(menu.children[itemIndexPosition], menu)
 				chrome.storage.sync.set({ udemy_playback_rate: newRate }, () => {
 					video.playbackRate = newRate
 				})
@@ -102,7 +34,7 @@ function initialize(video, menu, rateBtn) {
 
 	function populateItems() {
 		let iterations = 1 + (MAX_RATE - MIN_RATE) / INCREMENT
-		// give us a functor of length iterations to run reduce on
+		// give us an array of length iterations to run reduce on
 		let menuItems = Array(iterations).fill()
 		// construct a chunk of HTML to give to innerHTML of menu
 		let menuHtml = menuItems.reduce((acc, curr, index) => (acc += renderListItem(index)), '')
@@ -114,25 +46,9 @@ function initialize(video, menu, rateBtn) {
 			element.addEventListener('click', () => {
 				setPlaybackRate(MAX_RATE - index * INCREMENT)
 				// make item clicked the selected item
-				setMenuItemsHTML(element)
+				setMenuItemsHTML(element, menu)
 			})
 		})
-	}
-
-	function setMenuItemsHTML(element) {
-		// set all menu items to be unselected
-		Object.keys(menu.children).forEach(val => {
-			let el = menu.children[val]
-			el.setAttribute('aria-checked', false)
-			el.setAttribute('title', '')
-			el.children[0].innerHTML = ''
-			el.style.background = ''
-		})
-		// adjust click event menu item to be selected
-		element.setAttribute('aria-checked', true)
-		element.setAttribute('title', ', selected')
-		element.children[0].innerHTML = ', selected'
-		element.style.background = MENU_ITEM_HIGHLIGHT
 	}
 
 	video.addEventListener('loadeddata', getPlaybackRate)
@@ -140,9 +56,7 @@ function initialize(video, menu, rateBtn) {
 	video.addEventListener('ended', getPlaybackRate)
 
 	// clicking the playback rate display increments the rate by INCREMENT
-	rateBtn.addEventListener('click', () => {
-		setPlaybackRate()
-	})
+	rateBtn.addEventListener('click', () => setPlaybackRate())
 
 	populateItems()
 }
@@ -157,39 +71,77 @@ function renderListItem(index) {
 	return listItem
 }
 
-const getVideoElement = () => document.getElementsByTagName('video')[0]
+function setMenuItemsHTML(element, menu) {
+	// set all menu items to be unselected
+	Object.keys(menu.children).forEach(val => {
+		let el = menu.children[val]
+		el.setAttribute('aria-checked', false)
+		el.setAttribute('title', '')
+		el.children[0].innerHTML = ''
+		el.style.background = ''
+	})
+	// adjust click event menu item to be selected
+	element.setAttribute('aria-checked', true)
+	element.setAttribute('title', ', selected')
+	element.children[0].innerHTML = ', selected'
+	element.style.background = MENU_ITEM_HIGHLIGHT
+}
 
-const getMenuElement = () =>
-	document.querySelector(
-		'div.vjs-playback-rate.vjs-menu-button.vjs-menu-button-popup.vjs-control.vjs-button > div.vjs-menu > ul.vjs-menu-content',
-	)
+let videoToAdd = false
+let menuToAdd = false
+let rateBtnToAdd = false
 
-const getRateButton = () => document.getElementsByClassName('vjs-playback-rate-value')[0]
+let checkForClass = (node, classCheck, type) => {
+	if (type === 'video') {
+		if (!node.target.classList) return false
+		if (node.target.classList.contains(classCheck)) {
+			let firstChild = node.target.children[0]
+			if (firstChild) {
+				if (firstChild.classList.contains('vjs-tech')) {
+					videoToAdd = firstChild
+					return
+				}
+			}
+			return
+		}
+	}
 
-const getRequiredNodes = () => [getVideoElement(), getMenuElement(), getRateButton()]
+	if (node.children) {
+		let children = Array.from(node.children)
+		children.forEach(child => {
+			if (child.classList.contains(classCheck)) {
+				if (type === 'menu') {
+					if (!child.firstChild) return
+					if (child.firstChild.textContent === 'Speed') {
+						menuToAdd = child
+						return
+					}
+					return
+				}
+				rateBtnToAdd = child
+				return
+			}
+			return checkForClass(child, classCheck, type)
+		})
+	} else return
+}
 
 let Observer = new MutationObserver((mutations, observer) => {
-	mutations.forEach(() => {
-		let requiredNodes = getRequiredNodes()
-		// if all desired nodes exist
-		let documentReady = requiredNodes.every(el => !!el == true)
-		if (documentReady) {
-			observer.disconnect()
-			initialize(...requiredNodes)
+	mutations.forEach(mutation => {
+		checkForClass(mutation, 'video-js', 'video')
+
+		if (mutation.addedNodes.length > 0) {
+			let addedNodes = Array.from(mutation.addedNodes)
+			addedNodes.forEach(node => {
+				checkForClass(node, 'vjs-menu-content', 'menu')
+				checkForClass(node, 'vjs-playback-rate-value', 'rateBtn')
+			})
 		}
-		return
+		if (videoToAdd && menuToAdd && rateBtnToAdd) {
+			initialize(videoToAdd, menuToAdd, rateBtnToAdd)
+			videoToAdd = menuToAdd = rateBtnToAdd = false
+		}
 	})
 })
 
-function observeDocument() {
-	let nodes = getRequiredNodes()
-	// if not initial pageload manually remove elements so we can use mutation observer when new video
-	// is loaded - without this mutationObserver may complete on current elements instead of new elements
-	// filled out by Udemy AJAX request
-	nodes.forEach(node => {
-		if (node) node.remove()
-	})
-	Observer.observe(document, { childList: true, subtree: true })
-}
-
-observeDocument()
+Observer.observe(document, { childList: true, subtree: true })
