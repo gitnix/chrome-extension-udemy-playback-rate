27048f764035aa39192d69ef38dc668204680a78
Gitnix <gitnix1337@gmail.com>
2018-11-23 19:15:00 -0500

    made sure video initializes properly (check against last video), removed checkIfVideoSrc fn, converted appropriate variables to const

--- extension/playbackRate.js
+++ extension/playbackRate.js
@@ -4,18 +4,26 @@ const INCREMENT = 0.25
 
 const MENU_ITEM_HIGHLIGHT = 'rgb(236, 82, 82)'
 
-let currentVideo, currentMenu
+let currentVideo
+let currentMenu
 let keyListenerAdded = false
+let lastVideo = 'last'
+let videoToAdd = false
+let menuToAdd = false
+let rateBtnToAdd = false
 
-function initialize(video, menu, rateBtn) {
-	foundVideoSrc = true
+function initialize() {
+	const video = videoToAdd
+	const menu = menuToAdd
+	lastVideo = videoToAdd
+	videoToAdd = menuToAdd = false
 
 	function getPlaybackRate() {
 		currentVideo = video
 		currentMenu = menu
 		chrome.storage.sync.get({ udemy_playback_rate: 1 }, obj => {
 			// gotta love algebra
-			let itemIndexPosition =
+			const itemIndexPosition =
 				(obj.udemy_playback_rate - MAX_RATE) / INCREMENT / -1
 			setMenuItemsHTML(menu.children[itemIndexPosition], menu)
 			video.playbackRate = obj.udemy_playback_rate
@@ -23,18 +31,18 @@ function initialize(video, menu, rateBtn) {
 	}
 
 	function populateItems() {
-		let iterations = 1 + (MAX_RATE - MIN_RATE) / INCREMENT
+		const iterations = 1 + (MAX_RATE - MIN_RATE) / INCREMENT
 		// give us an array of length iterations to run reduce on
-		let menuItems = Array(iterations).fill()
+		const menuItems = Array(iterations).fill()
 		// construct a chunk of HTML to give to innerHTML of menu
-		let menuHtml = menuItems.reduce(
+		const menuHtml = menuItems.reduce(
 			(acc, curr, index) => (acc += renderListItem(index)),
 			'',
 		)
 
 		menu.innerHTML = menuHtml
 		Object.keys(menu.children).forEach((val, index) => {
-			let element = menu.children[val]
+			const element = menu.children[val]
 			// menu item will change playback rate to its value when clicked
 			element.addEventListener('click', () => {
 				setPlaybackRate(MAX_RATE - index * INCREMENT, video, menu)
@@ -62,7 +70,7 @@ function setPlaybackRate(rate, video, menu) {
 			if (rate === 'decrease') newRate = obj.udemy_playback_rate - INCREMENT
 			if (newRate > MAX_RATE) newRate = MIN_RATE
 			if (newRate < MIN_RATE) newRate = MAX_RATE
-			let itemIndexPosition = (newRate - MAX_RATE) / INCREMENT / -1
+			const itemIndexPosition = (newRate - MAX_RATE) / INCREMENT / -1
 			setMenuItemsHTML(menu.children[itemIndexPosition], menu)
 			chrome.storage.sync.set({ udemy_playback_rate: newRate }, () => {
 				video.playbackRate = newRate
@@ -75,8 +83,8 @@ function setPlaybackRate(rate, video, menu) {
 }
 
 function renderListItem(index) {
-	let playbackNumber = MAX_RATE - INCREMENT * index
-	let listItem = `
+	const playbackNumber = MAX_RATE - INCREMENT * index
+	const listItem = `
 		<li tabindex="-1" role="menuitem" aria-live="polite" aria-disabled="false" aria-checked="false">
 			${playbackNumber}x
 			<span class="vjs-control-text"></span>
@@ -87,7 +95,7 @@ function renderListItem(index) {
 function setMenuItemsHTML(element, menu) {
 	// set all menu items to be unselected
 	Object.keys(menu.children).forEach(val => {
-		let el = menu.children[val]
+		const el = menu.children[val]
 		el.setAttribute('aria-checked', false)
 		el.setAttribute('title', '')
 		el.children[0].innerHTML = ''
@@ -100,31 +108,11 @@ function setMenuItemsHTML(element, menu) {
 	element.style.background = MENU_ITEM_HIGHLIGHT
 }
 
-let videoToAdd = false
-let menuToAdd = false
-let rateBtnToAdd = false
-let foundVideoSrc = false
-
-function checkIfVideoSrc(videoId, menu, rateBtn) {
-	if (
-		!document.getElementById(videoId) ||
-		!document.getElementById(videoId).src
-	) {
-		if (foundVideoSrc) return
-		setTimeout(
-			() => checkIfVideoSrc(document.getElementById(videoId), menu, rateBtn),
-			2000,
-		)
-	} else {
-		initialize(document.getElementById(videoId), menu, rateBtn)
-	}
-}
-
-let checkForClass = (node, classCheck, type) => {
+function checkForClass(node, classCheck, type) {
 	if (type === 'video') {
-		if (!node.target.classList) return false
+		if (!node.target.classList) return
 		if (node.target.classList.contains(classCheck)) {
-			let firstChild = node.target.children[0]
+			const firstChild = node.target.children[0]
 			if (firstChild) {
 				if (firstChild.classList.contains('vjs-tech')) {
 					videoToAdd = firstChild
@@ -136,7 +124,7 @@ let checkForClass = (node, classCheck, type) => {
 	}
 
 	if (node.children) {
-		let children = Array.from(node.children)
+		const children = Array.from(node.children)
 		children.forEach(child => {
 			if (child.classList.contains(classCheck)) {
 				if (type === 'menu') {
@@ -160,24 +148,21 @@ let checkForClass = (node, classCheck, type) => {
 	} else return
 }
 
-let Observer = new MutationObserver((mutations, observer) => {
+const Observer = new MutationObserver(mutations => {
 	mutations.forEach(mutation => {
 		checkForClass(mutation, 'video-js', 'video')
 
 		if (mutation.addedNodes.length > 0) {
-			let addedNodes = Array.from(mutation.addedNodes)
+			const addedNodes = Array.from(mutation.addedNodes)
 			addedNodes.forEach(node => {
 				checkForClass(node, 'vjs-menu-content', 'menu')
 				checkForClass(node, 'vjs-playback-rate-value', 'rateBtn')
 			})
 		}
 		if (videoToAdd && menuToAdd && rateBtnToAdd) {
-			let videoToUse = videoToAdd
-			let menuToUse = menuToAdd
-			let rateBtnToUse = rateBtnToAdd
-			videoToAdd = menuToAdd = rateBtnToAdd = false
-			foundVideoSrc = false
-			checkIfVideoSrc(videoToUse.id, menuToUse, rateBtnToUse)
+			if (videoToAdd !== lastVideo) {
+				initialize()
+			}
 		}
 	})
 })
